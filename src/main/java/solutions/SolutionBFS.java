ef4d57659f89025cf49943d8f9f271479e40f718
nachogl1 <i.gonzalezl@hotmail.com>
2025-03-18 14:04:11 +0000

    Solution: BFS done

---
+++ src/main/java/solutions/SolutionBFS.java
@@ -0,0 +1,38 @@
+package solutions;
+
+import model.Node;
+import utils.Console;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class SolutionBFS implements Solution {
+    private final Console console;
+
+    public SolutionBFS(Console console) {
+        this.console = console;
+    }
+
+    @Override
+    public boolean search(Node tree, String searchTerm) {
+        return recursiveSearch(tree, searchTerm);
+    }
+
+    private boolean recursiveSearch(Node node, String searchTerm) {
+        //Due to the nature of BFS (Queues) using recursion (STACK) is not the best option
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            node = queue.poll();
+
+            console.print(node.getValue());
+
+            if (searchTerm.equals(node.getValue())) return true;
+            if (node.getLeft() != null) queue.add(node.getLeft());
+            if (node.getRight() != null) queue.add(node.getRight());
+        }
+        return false;
+
+    }
+}
