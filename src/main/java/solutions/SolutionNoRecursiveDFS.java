f4698957303621b0854d7d7c4ad8a056a934f83e
nachogl1 <i.gonzalezl@hotmail.com>
2025-03-18 14:13:22 +0000

    Solution: Non recursive PreOrder DFS done

---
+++ src/main/java/solutions/SolutionNoRecursiveDFS.java
@@ -0,0 +1,38 @@
+package solutions;
+
+import model.Node;
+import utils.Console;
+
+import java.util.Stack;
+
+public class SolutionNoRecursiveDFS implements Solution {
+    private final Console console;
+
+    public SolutionNoRecursiveDFS(Console console) {
+        this.console = console;
+    }
+
+    @Override
+    public boolean search(Node tree, String searchTerm) {
+        return recursiveSearch(tree, searchTerm);
+    }
+
+    private boolean recursiveSearch(Node node, String searchTerm) {
+        if(node == null) return false;
+
+        Stack<Node> stack = new Stack<>();
+        stack.add(node);
+
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+
+            console.print(node.getValue());
+
+            if (searchTerm.equals(node.getValue())) return true;
+            if (node.getRight() != null) stack.add(node.getRight());
+            if (node.getLeft() != null) stack.add(node.getLeft());
+        }
+        return false;
+
+    }
+}
